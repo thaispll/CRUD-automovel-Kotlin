d4b57bf94cd453eec99f1f64f360e42da4b554ea
Thais Leal <thais.pll@hotmail.com>
2025-05-30 21:27:34 -0300

    Adicionando + telas

--- app/src/main/java/br/com/codeschool/crud_automovel/view/LoginActivity.kt
+++ app/src/main/java/br/com/codeschool/crud_automovel/view/LoginActivity.kt
@@ -1,4 +1,16 @@
 package br.com.codeschool.crud_automovel.view
 
-class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity
+
+//Apresenta tela de login do app
+class LoginActivity: AppCompatActivity() {
+    //Variável para acessar os elementos da interface via ViewBinding
+    private lateinit var binding: ActivityLoginBinding // binding = vinculacao
+
+    //Variável para o ViewModel que gerencia a autenticação
+    private lateinit var viewModel: AuthViewModel //viewmodel -modelo de autenticação
+
+
+
+
 }
